a1eea5e335ec391356945c0df1698d858d6211a8
samjwu <sjwu@ualberta.ca>
2024-01-21 16:13:33 -0700

    Add constants

---
+++ js/constants.js
@@ -0,0 +1,5 @@
+const EDUCATION_DATA = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
+const COUNTY_DATA = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
+
+const SVG_WIDTH = 1000;
+const SVG_HEIGHT = 600;
